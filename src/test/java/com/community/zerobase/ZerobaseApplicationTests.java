8c9a4d794bcfc4ba0fcd8f46ee44fd22e014a95d
고성태 <v12rhtjdxo@naver.com>
2024-05-16 15:50:08 +0900

    first commit

---
+++ src/test/java/com/community/zerobase/ZerobaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.community.zerobase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerobaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
